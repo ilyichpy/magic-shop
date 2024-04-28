7017ab4103a52b574fa119a36711b1e8c0d892f3
ilyichpy <ilyaszuev25@gmail.com>
2024-04-28 16:54:26 +0300

    Настроил первый объект и конфиг для подключения к базе данных с помощью переменных окружения

---
+++ magic-shop-crud/src/main/java/ru/ilya/zuev/magic/crud/dto/MagicWandEntity.java
@@ -0,0 +1,39 @@
+package ru.ilya.zuev.magic.crud.dto;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "MAGIC_WAND")
+public class MagicWandEntity {
+	@Id
+	@GeneratedValue(generator = "WAND_ID_SEQ", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "WAND_ID_SEQ", sequenceName = "WAND_ID_SEQ")
+	@Column(name = "ID")
+	Long id;
+
+	/*
+	* Имя палочки
+	*/
+	@Column(name = "WAND_NAME")
+	String name;
+
+	/*
+	* Длина палочки
+	*/
+	@Column(name = "LENGTH")
+	double length;
+
+	/*
+	* Ширина палочки
+	*/
+	@Column(name = "WIDTH")
+	double width;
+
+}
