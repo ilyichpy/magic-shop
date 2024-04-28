ec95c75d9fc965357d55a164db28308e79d1482e
ilyichpy <ilyaszuev25@gmail.com>
2024-04-28 11:43:31 +0300

    init commit

---
+++ magic-shop-crud/src/test/java/ru/ilya/zuev/magic/MagicShopCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilya.zuev.magic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicShopCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
